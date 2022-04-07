5f38f20f10528155062619de751879c65c5902da
Pourmoslemi <102228292+Pourmoslemi@users.noreply.github.com>
2022-04-07 17:57:33 +0200

    Update DirTreeWalk.py

--- DirTreeWalk.py
+++ DirTreeWalk.py
@@ -3,12 +3,15 @@ import hashlib
 import os
 from os.path import join
 
+# Create the parser
 parser = argparse.ArgumentParser()
+# Add the arguments
 parser.add_argument('path', type=str, help='path to show')
+# Execute the parse_args() method
 args = parser.parse_args()
 
 
-def printDirectory(f, f_path):
+def dirtreewalk(f, f_path):
     global sub_files
     fullpath = join(f_path, f)
     if os.path.isfile(fullpath):
@@ -21,7 +24,7 @@ def printDirectory(f, f_path):
         printBeautiful(file_info)
         sub_files = os.listdir(fullpath)
     for subFile in sub_files:
-        printDirectory(subFile, fullpath)
+        dirtreewalk(subFile, fullpath)
 
 
 def printBeautiful(fileInfo):
@@ -29,14 +32,17 @@ def printBeautiful(fileInfo):
 
 
 if __name__ == '__main__':
-    path = args.path
-    try:
-        dirs = os.listdir(path)
-        for file in dirs:
-            printDirectory(file, path)
-    except PermissionError:
-        print('Sie haben keine Berechtiung!')
-    except NotADirectoryError:
-        print("mich_gibt_es_nicht!")
-    except FileNotFoundError:
-        print("mich_gibt_es_nicht!")
+    input_path = args.path
+    if not os.path.isdir(input_path):
+        print('mich_gibt_es_nicht!')
+    else:
+        try:
+            dirs = os.listdir(input_path)
+            for file in dirs:
+                dirtreewalk(file, input_path)
+        except PermissionError:
+            print('Sie haben keine Berechtiung!')
+        except NotADirectoryError:
+            print("mich_gibt_es_nicht!")
+        except FileNotFoundError:
+            print("mich_gibt_es_nicht!")
